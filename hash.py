536331df9e9ea0504a46af492582dd4d69543ea3
puneet <puneet@bito.ltd>
2020-04-23 00:12:44 -0400

    cs253 course: added password hashing using salt method

--- hash.py
+++ hash.py
@@ -1,7 +1,14 @@
 import hashlib
 import hmac
+import random
+import string
 
 SECRET = b'iamsosecret'
+
+# returns a string of 5 random characters
+def make_salt():
+    return ''.join(random.choice(string.ascii_letters) for x in range(5))
+
 # method to create a hash key for an input
 def hash_str(s):
     return hmac.new(SECRET, str(s).encode()).hexdigest()
@@ -15,3 +22,16 @@ def check_secure_val(h):
     val = h.split('|')[0]
     if val.isdigit() and make_secure_val(val) == h:
         return val
+# returns a hashed (sha256) password of the format:
+# HASH(name + pw + salt), salt
+def make_password_hash(name, pw, salt=make_salt()):
+    return "%s|%s" %(hashlib.sha256((name+pw+salt).encode()).hexdigest(), salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split('|')[0]
+    should_be_hash = make_password_hash(name, pw, salt)
+    if should_be_hash == h:
+        return True
+    else:
+        return False
+
